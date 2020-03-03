4d4852ff8bd15ca078c95c04b6327ffd45f51b2c
Igor Pahota <eto.pochtovaya@gmail.com>
2020-03-03 18:16:19 +0300

    email validation in login form added

--- vue-front/src/views/Login.vue
+++ vue-front/src/views/Login.vue
@@ -1,15 +1,24 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
           id="email"
           type="text"
-          class="validate"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) ||
+          ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >Поле e-mail не должно быть пустым</small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >Введите корректный e-mail</small>
       </div>
       <div class="input-field">
         <input
@@ -39,3 +48,36 @@
     </div>
   </form>
 </template>
+
+
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'Login',
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+  },
+
+  methods: {
+    submitHandler() {
+      // console.log(this.$v.password.$params);
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+      console.log(formData);
+      this.$router.push('/');
+    },
+  },
+};
+</script>
