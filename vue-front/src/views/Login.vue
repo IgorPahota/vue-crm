a79205b2980c78a198a2781f307c16a85b12d0fe
Igor Pahota <eto.pochtovaya@gmail.com>
2020-03-04 18:41:30 +0300

    added firebase and login feature with test@mail.ru/123456 login form

--- vue-front/src/views/Login.vue
+++ vue-front/src/views/Login.vue
@@ -77,7 +77,7 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -86,8 +86,13 @@ export default {
         email: this.email,
         password: this.password,
       };
-      console.log(formData);
-      this.$router.push('/');
+      try {
+        await this.$store.dispatch('login', formData);
+        await this.$router.push('/');
+      } catch (e) {
+        console.log(e.message)
+      }
+
     },
   },
 };
