de253c99ca381529b6bb73776e2a02a631b79f2f
Igor Pahota <eto.pochtovaya@gmail.com>
2020-03-06 18:10:41 +0300

    added HomeBill.vue and HomeCurrency.vue without logic

---
+++ vue-front/src/components/HomeBill.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "HomeBill"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-front/src/components/HomeCurrency.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "HomeCurrency"
+    }
+</script>
+
+<style scoped>
+
+</style>
